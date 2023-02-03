440bb9353a78a5e16b5d71902d22ab9148068d84
loremcodes <prasadjunghare2@gmail.com>
2023-02-03 18:27:54 +0530

    03 sidebar added close icon

--- 03Sidebar/Js/app.js
+++ 03Sidebar/Js/app.js
@@ -1,9 +1,20 @@
 // catch the sidebar
 const menu = document.getElementById('menu')
 const button = document.getElementById('icon')
+const closeIcon = document.getElementById('close-icon')
 
 button.addEventListener('click', function () {
     console.log(`I was clicked`);
+    let sidebar = menu.style.display;
+    if (sidebar == 'none') {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+
+})
+
+closeIcon.addEventListener('click', function () {
     let sidebar = menu.style.display;
     if (sidebar == 'none') {
         menu.style.display = 'block'
