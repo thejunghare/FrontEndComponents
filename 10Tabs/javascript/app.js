53a6800783a735d3c455a3c077c91b79d180ee63
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-09-22 17:56:07 +0530

    fixed tabs switching app.js

--- 10Tabs/javascript/app.js
+++ 10Tabs/javascript/app.js
@@ -1,20 +1,22 @@
-function openCity(evt, cityName) {
+document.getElementById("defaultOpen").click();
+
+        function openCity(evt, name) {
             // Declare all variables
-            var i, tabcontent, tablinks;
+            var i, tabcontent, link;
 
             // Get all elements with class="tabcontent" and hide them
-            tabcontent = document.getElementsByClassName("tabcontent");
+            tabcontent = document.getElementsByClassName("tab-content");
             for (i = 0; i < tabcontent.length; i++) {
                 tabcontent[i].style.display = "none";
             }
 
-            // Get all elements with class="tablinks" and remove the class "active"
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            // Get all elements with class="link" and remove the class "active"
+            link = document.getElementsByClassName("link");
+            for (i = 0; i < link.length; i++) {
+                link[i].className = link[i].className.replace(" active", "");
             }
 
             // Show the current tab, and add an "active" class to the button that opened the tab
-            document.getElementById(cityName).style.display = "block";
+            document.getElementById(name).style.display = "block";
             evt.currentTarget.className += " active";
         }
