5f6cda2bf759a6bfb26d02d9c7ead64c2235d0ce
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-10-16 17:28:58 +0530

    Create lang-local-storage.js

---
+++ working-with-cookies/lang-local-storage.js
@@ -0,0 +1,8 @@
+/* We are using the local storage here as it has it's own benefits */
+
+
+// get the selected lang
+var selectedLanguage = localStorage.getItem('selectedLanguage')
+
+// set the lang
+localStorage.setItem('selectedLanguage', lang);
