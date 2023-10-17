65550c7e84f13db738de1e9c139c32196d548935
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-10-17 12:02:00 +0530

    add function to copy attribute

--- add-multiple-element/app.js
+++ add-multiple-element/app.js
@@ -1,23 +1,47 @@
 'use strict'
 
-const addAnotherElement = () => {
-    const inputContainer = document.getElementById('inputContainer');
+const copy = (sourceElement, targetElement) => {
+    
+        // copy style attribute
+        let styles = sourceElement.getAttribute('class')
+        targetElement.setAttribute("class", styles)
+    
+        // copy name attribute
+        let name = sourceElement.getAttribute('name')
+        targetElement.setAttribute('name', name)
+   
 
-    // Create a new input element of type 'file'
-    const inputFile = document.createElement('input');
-    inputFile.type = 'file';
-
-    // Create a new input element of type 'text '
-    const inputText = document.createElement('input');
-    inputText.type = 'text';
-
-    // Append the new input to the container
-    inputContainer.appendChild(inputText);
-    inputContainer.appendChild(inputFile);
+    /* 
+        // more optimized way to do the same
+        const attributeToCopy = ['class', 'name']
+        attributeToCopy.forEach(attribute => {
+            const value = sourceElement.getAttribute(attribute)
+            if (value !== null) {
+                targetElement.setAttribute(attribute, value)
+            }
+        }) 
+    */
 }
 
+const addAnotherElement = () => {
+    const inputContainer = document.getElementById('inputContainer')
+
+    // Create a new input element of type 'file'
+    const duplicateFileInput = document.createElement('input')
+    duplicateFileInput.type = 'file'
+    copy(document.getElementById('fileInput'), duplicateFileInput)
+
+    // Create a new input element of type 'text'
+    const duplicateTextInput = document.createElement('input')
+    duplicateTextInput.type = 'text'
+    copy(document.getElementById('textInput'), duplicateTextInput)
+
+    // Append the new input elements to the container
+    inputContainer.appendChild(duplicateTextInput)
+    inputContainer.appendChild(duplicateFileInput)
+};
 
 const addButton = document.getElementById('addInput')
 if (addButton) {
-    addButton.addEventListener('click', addAnotherElement);
+    addButton.addEventListener('click', addAnotherElement)
 }
