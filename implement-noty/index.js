540df07e2c4ec7aeca3a56a846ac998b50c088d9
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-10-19 19:01:14 +0530

    Add implementation of noty alerts

---
+++ implement-noty/index.js
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <!-- noty css -->
+    <link rel="stylesheet" type="text/css" href="node_modules/noty/lib/noty.css">
+    <!-- noty theme -->
+    <link rel="stylesheet" type="text/css" href="node_modules/noty/lib/themes/sunset.css">
+    <style>
+        .noty_effects_open {
+            opacity: 0;
+            transform: translate(50%);
+            animation: noty_anim_in 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        }
+
+        .noty_effects_close {
+            animation: noty_anim_out 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        }
+
+        @keyframes noty_anim_in {
+            100% {
+                transform: translate(0);
+                opacity: 1;
+            }
+        }
+
+        @keyframes noty_anim_out {
+            100% {
+                transform: translate(50%);
+                opacity: 0;
+            }
+        }
+    </style>
+</head>
+<body>
+<div class="m-5">
+    <button id="showAlertButton" class="btn btn-primary btn-sm">Show Alert</button>
+</div>
+<script src="node_modules/noty/lib/noty.js"></script>
+
+<script>
+    const showAlert = () => {
+        /* console.log(`I was called`)*/
+        new Noty({
+            theme: 'sunset',
+            type: 'success',
+            text: 'NOTY - a dependency-free notification library!',
+            animation: {
+                open: 'animated bounceInRight noty_effects_open', // Animate.css class names
+                close: 'animated bounceOutRight noty_effects_close' // Animate.css class names
+            }
+        }).show();
+    }
+
+    // Add a click event listener to the button
+    document.getElementById('showAlertButton').addEventListener('click', showAlert);
+</script>
+</body>
+</html>
